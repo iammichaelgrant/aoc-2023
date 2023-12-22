27853341a5d70ef8b47b9d7b002b3fc8e820db3b
mgrant <michael.paul.grant@gmail.com>
2023-12-22 14:03:40 +0000

    day 6 - part 1

---
+++ src/aoc_2023/day_6.clj
@@ -0,0 +1,30 @@
+(ns aoc-2023.day-6
+  (:require [clojure.string :as str]))
+
+(def input (slurp "resources/inputs/day_6.txt"))
+
+;; Part 1
+
+(def sample-input "Time:      7  15   30\nDistance:  9  40  200")
+
+(defn parse-input [input]
+  (let [[times distances] (->> input
+                               str/split-lines
+                               (map #(-> %
+                                         (str/split #"( +)")
+                                         rest)))]
+    (map #(hash-map :time (parse-long %1)
+                    :distance (parse-long %2))
+         times
+         distances)))
+
+(defn winning-distances [{:keys [time distance]}]
+  (let [possible-distances (map #(* % (- time %)) (range 0 time))
+        winning-distances (filter #(< distance %) possible-distances)]
+    winning-distances))
+
+(->> input
+     parse-input
+     (map winning-distances)
+     (map count)
+     (reduce *))
